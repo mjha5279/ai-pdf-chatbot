25d55bc0774b7655e0885713914ddb382e111748
Prachi Varshney <varshneyprachi26@gmail.com>
2025-11-24 19:47:04 +0530

    Delete model.py

--- model.py
+++
@@ -1,7 +0,0 @@
-import google.generativeai as genai
-
-genai.configure(api_key="AIzaSyD2xR45EOdM6N01COQ8FemYPG3A823Pa_0")
-model = genai.GenerativeModel("models/gemini-2.5-flash")
-
-response = model.generate_content("Hello, test message")
-print(response.text)
